bc5ba3b808c37a4ec17c8226da4a04eec39629df
vinyl-umbrella <61788251+vinyl-umbrella@users.noreply.github.com>
2024-10-22 00:46:36 +0900

    fix

--- cloud/lambda-to-chatbot/src/notify.py
+++ cloud/lambda-to-chatbot/src/notify.py
@@ -8,6 +8,7 @@ import boto3
 def main(event, context):
     l = logging.getLogger(__name__)
     l.info(event)
+    msg = json.loads(event["Records"][0]["Sns"]["Message"])
 
     sns = boto3.client("sns")
 
@@ -15,8 +16,8 @@ def main(event, context):
         "version": "1.0",
         "source": "custom",
         "content": {
-            "title": f':warning: {event["requestContext"]["functionArn"]} failed',
-            "description": f'Error: {event["responsePayload"]["errorMessage"]}\n```{event["responsePayload"]["stackTrace"]}```',
+            "title": f':warning: {msg["requestContext"]["functionArn"]} failed',
+            "description": f'Error: {msg["responsePayload"]["errorMessage"]}\n```{msg["responsePayload"]["stackTrace"]}```',
         },
     }
 
