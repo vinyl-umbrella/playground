0461c1a949b4379fb28cc3d5b586d6898869ae01
vinyl-umbrella <61788251+vinyl-umbrella@users.noreply.github.com>
2025-02-15 19:12:52 +0900

    add sample cd

---
+++ aws/sam-cicd/sample-python/src/main.py
@@ -0,0 +1,12 @@
+import logging
+
+
+def lambda_handler():
+    logger = logging.getLogger(__name__)
+    logger.info("Hello World")
+    logger.warning("This is a warning")
+    logger.error("This is an error")
+
+
+if __name__ == "__main__":
+    lambda_handler()
