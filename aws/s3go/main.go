fb10b67ffd0ef15ff8675e5ef853dad9463b1d90
vinyl-umbrella <61788251+vinyl-umbrella@users.noreply.github.com>
2025-08-02 20:26:45 +0900

    s3 stream download

---
+++ aws/s3go/main.go
@@ -0,0 +1,151 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+type BucketBasics struct {
+	S3Client   *s3.Client
+	Downloader *manager.Downloader
+}
+
+func (b *BucketBasics) StreamDL(ctx context.Context, bucketName, objectKey, filename string) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return fmt.Errorf("failed to create file: %w", err)
+	}
+	defer file.Close()
+
+	// stream downlaod
+	_, err = b.Downloader.Download(ctx, file, &s3.GetObjectInput{
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(objectKey),
+	})
+	if err != nil {
+		return fmt.Errorf("failed to download object: %w", err)
+	}
+
+	if fileInfo, err := os.Stat(filename); err == nil {
+		log.Printf("Downloaded %d bytes to %s", fileInfo.Size(), filename)
+	}
+
+	return nil
+}
+
+func simpleGetObject(ctx context.Context, s3Client *s3.Client, bucketName, objectKey, filename string) error {
+	result, err := s3Client.GetObject(ctx, &s3.GetObjectInput{
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(objectKey),
+	})
+	if err != nil {
+		return fmt.Errorf("failed to download object: %w", err)
+	}
+	defer result.Body.Close()
+
+	file, err := os.Create(filename)
+	if err != nil {
+		return fmt.Errorf("failed to create file: %w", err)
+	}
+
+	_, err = io.Copy(file, result.Body)
+	if err != nil {
+		return fmt.Errorf("failed to copy object data: %w", err)
+	}
+	defer file.Close()
+
+	return nil
+}
+
+// 単純な S3 GetObject を実行する関数
+func badGetObject(ctx context.Context, s3Client *s3.Client, bucketName, objectKey, filename string) error {
+	result, err := s3Client.GetObject(ctx, &s3.GetObjectInput{
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(objectKey),
+	})
+	if err != nil {
+		return fmt.Errorf("failed to download object: %w", err)
+	}
+	defer result.Body.Close()
+
+	fileSize := result.ContentLength
+	buffer := make([]byte, *fileSize) // とっても悪い例
+
+	totalRead := 0
+	for {
+		n, err := result.Body.Read(buffer[totalRead:])
+		totalRead += n
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return fmt.Errorf("failed to read response body: %w", err)
+		}
+	}
+
+	file, err := os.Create(filename)
+	if err != nil {
+		return fmt.Errorf("failed to create file: %w", err)
+	}
+	defer file.Close()
+
+	_, err = file.Write(buffer[:totalRead])
+	if err != nil {
+		return fmt.Errorf("failed to write to file: %w", err)
+	}
+
+	return nil
+}
+
+func main() {
+	// load bucket, key from argv
+	if len(os.Args) < 4 {
+		log.Fatalf("Usage: %s <bucket> <key> <download_file>", os.Args[0])
+	}
+	bucketName := os.Args[1]
+	objectKey := os.Args[2]
+	downloadFile := os.Args[3]
+
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		log.Fatalf("Failed to load AWS config: %v", err)
+	}
+	s3Client := s3.NewFromConfig(cfg, func(o *s3.Options) {
+		o.UsePathStyle = false
+	})
+	downloader := manager.NewDownloader(s3Client, func(d *manager.Downloader) {
+		d.PartSize = 10 * 1024 * 1024 // 10 MiB
+		d.Concurrency = 4             // 4並列
+	})
+
+	bucketBasics := &BucketBasics{
+		S3Client:   s3Client,
+		Downloader: downloader,
+	}
+
+	ctx := context.Background()
+
+	log.Printf("Starting download of %s/%s to %s", bucketName, objectKey, downloadFile)
+
+	// ファイルにダウンロード
+	err = bucketBasics.StreamDL(ctx, bucketName, objectKey, downloadFile)
+	if err != nil {
+		log.Printf("Failed to download file: %v\n", err)
+		return
+	}
+
+	log.Printf("Successfully downloaded file: %s\n", downloadFile)
+
+	// ダウンロードしたファイルの情報を表示
+	if fileInfo, err := os.Stat(downloadFile); err == nil {
+		log.Printf("Downloaded file size: %.2f MB", float64(fileInfo.Size())/(1024*1024))
+	}
+}
---
+++ aws/s3go/main_test.go
@@ -0,0 +1,209 @@
+package main
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+// テスト用の設定を環境変数から取得
+func getTestConfig() (string, string, bool) {
+	bucketName := os.Getenv("S3_BUCKET_NAME")
+	objectKey := os.Getenv("S3_OBJECT_KEY")
+
+	if bucketName == "" || objectKey == "" {
+		return "", "", false
+	}
+
+	return bucketName, objectKey, true
+}
+
+// ベンチマーク用のダウンローダーを作成
+func createBenchmarkDownloader(partSize int64, concurrency int) (*BucketBasics, error) {
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		return nil, err
+	}
+
+	s3Client := s3.NewFromConfig(cfg)
+
+	downloader := manager.NewDownloader(s3Client, func(d *manager.Downloader) {
+		d.PartSize = partSize
+		d.Concurrency = concurrency
+	})
+
+	return &BucketBasics{
+		S3Client:   s3Client,
+		Downloader: downloader,
+	}, nil
+}
+
+// チャンクサイズ別ベンチマーク (チャンクサイズ: [512KB, 1MB, 2MB, 5MB, 10MB], 並行数: 1)
+func BenchmarkS3DownloadPartSize(b *testing.B) {
+	bucketName, objectKey, _ := getTestConfig()
+
+	testCases := []struct {
+		name     string
+		partSize int64
+	}{
+		// {"512KB", 512 * 1024},
+		// {"1MB", 1024 * 1024},
+		// {"2MB", 2 * 1024 * 1024},
+		// {"5MB", 5 * 1024 * 1024},
+		// {"10MB", 10 * 1024 * 1024},
+		{"20MB", 20 * 1024 * 1024},
+		{"50MB", 50 * 1024 * 1024},
+		{"100MB", 100 * 1024 * 1024},
+		{"200MB", 200 * 1024 * 1024},
+	}
+
+	for _, tc := range testCases {
+		b.Run(tc.name, func(b *testing.B) {
+			bucketBasics, err := createBenchmarkDownloader(tc.partSize, 16)
+			if err != nil {
+				b.Fatalf("Failed to create downloader: %v", err)
+			}
+
+			tempFile := "/tmp/benchmark_part_" + tc.name
+			defer os.Remove(tempFile)
+
+			ctx := context.Background()
+
+			b.ResetTimer()
+			b.ReportAllocs()
+
+			for i := 0; i < b.N; i++ {
+				os.Remove(tempFile)
+
+				err := bucketBasics.StreamDL(ctx, bucketName, objectKey, tempFile)
+				if err != nil {
+					b.Errorf("Download failed: %v", err)
+				}
+			}
+		})
+	}
+}
+
+// 並行数別ベンチマーク (チャンクサイズ: 1MiB, 並行数: [2^0, 2^1, 2^2, 2^3, 2^4])
+func BenchmarkS3DownloadConcurrency(b *testing.B) {
+	bucketName, objectKey, _ := getTestConfig()
+
+	testCases := []struct {
+		name        string
+		concurrency int
+	}{
+		{"Seq", 1},
+		{"Par2", 2},
+		{"Par4", 4},
+		{"Par8", 8},
+		{"Par16", 16},
+	}
+
+	for _, tc := range testCases {
+		b.Run(tc.name, func(b *testing.B) {
+			bucketBasics, err := createBenchmarkDownloader(1024*1024, tc.concurrency)
+			if err != nil {
+				b.Fatalf("Failed to create downloader: %v", err)
+			}
+
+			tempFile := "/tmp/benchmark_conc_" + tc.name
+			defer os.Remove(tempFile)
+
+			ctx := context.Background()
+
+			b.ResetTimer()
+			b.ReportAllocs()
+
+			for i := 0; i < b.N; i++ {
+				os.Remove(tempFile)
+
+				err := bucketBasics.StreamDL(ctx, bucketName, objectKey, tempFile)
+				if err != nil {
+					b.Errorf("Download failed: %v", err)
+				}
+			}
+		})
+	}
+}
+
+// 速度重視の設定 (チャンクサイズ: 10MiB、並行数: 16)
+func BenchmarkS3DownloadSpeedOptimized(b *testing.B) {
+	bucketName, objectKey, _ := getTestConfig()
+
+	bucketBasics, err := createBenchmarkDownloader(10*1024*1024, 16)
+	if err != nil {
+		b.Fatalf("Failed to create downloader: %v", err)
+	}
+
+	tempFile := "/tmp/benchmark_speed_optimized"
+	defer os.Remove(tempFile)
+
+	ctx := context.Background()
+
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	for i := 0; i < b.N; i++ {
+		os.Remove(tempFile)
+
+		err := bucketBasics.StreamDL(ctx, bucketName, objectKey, tempFile)
+		if err != nil {
+			b.Errorf("Download failed: %v", err)
+		}
+	}
+}
+
+func BenchmarkSimpleS3GetObject(b *testing.B) {
+	bucketName, objectKey, _ := getTestConfig()
+
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		b.Fatalf("Failed to load AWS config: %v", err)
+	}
+	s3Client := s3.NewFromConfig(cfg)
+
+	tempFile := "/tmp/benchmark_get_object"
+	defer os.Remove(tempFile)
+
+	ctx := context.Background()
+
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	for i := 0; i < b.N; i++ {
+		err := simpleGetObject(ctx, s3Client, bucketName, objectKey, tempFile)
+		if err != nil {
+			b.Errorf("GetObject failed: %v", err)
+		}
+	}
+}
+
+// GetObject 後に大きなバッファを確保している悪い例
+func BenchmarkBadS3GetObject(b *testing.B) {
+	bucketName, objectKey, _ := getTestConfig()
+
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		b.Fatalf("Failed to load AWS config: %v", err)
+	}
+	s3Client := s3.NewFromConfig(cfg)
+
+	tempFile := "/tmp/benchmark_get_object"
+	defer os.Remove(tempFile)
+
+	ctx := context.Background()
+
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	for i := 0; i < b.N; i++ {
+		err := badGetObject(ctx, s3Client, bucketName, objectKey, tempFile)
+		if err != nil {
+			b.Errorf("GetObject failed: %v", err)
+		}
+	}
+}
